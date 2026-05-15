1068ea229f26f90ef3cb5173bf80ada6e310fb11
Sripranav Nagabhatla <sripranavnagabhatla@gmail.com>
2026-05-15 20:15:37 +0530

    Feature Addition: Hint System added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import random
 print("Welcome to the number guessing game!")
 user_num = int(input("Please enter a number between 1 to 10: "))
-sys_num = random.randint(1,10)
-if user_num == sys_num:
-    print("CORRECT! You guessed the number.")
-else:
-    print("WRONG! The correct number was", sys_num)
+sys_num = random.randint(1, 10)
+while user_num != sys_num:
+    if user_num > sys_num:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
+    user_num = int(input("Please enter a number between 1 to 10: "))    
+print("Congratulations! You guessed the number correctly.")
